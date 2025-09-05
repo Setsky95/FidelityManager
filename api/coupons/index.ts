f14e41722502165b720eb251bd09af0c2f21b1cb
Sebastian Pavlotsky <sebapavlotsky@gmail.com>
2025-09-05 19:30:14 -0300

    api changes

---
+++ api/coupons/index.ts
@@ -0,0 +1,4 @@
+// Reexporta el handler principal de /api/coupons.ts
+// OJO: usando "NodeNext" conviene poner la extensión .js en el import
+import baseHandler from "../coupons.js";
+export default baseHandler;
